f1e4aa966626903b518067e6783d12104305ee05
Ryan Gregg <rgregg@users.noreply.github.com>
2016-03-02 23:27:12 -0800

    Move stuff around

---
+++ O3653/O3653-3 Deep Dive into Office 365 APIs for OneDrive for Business/Completed Projects/ADALVersion/src/GraphFilesWeb/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Authentication.Cookies;
+using Microsoft.AspNet.Authentication.OpenIdConnect;
+using Microsoft.AspNet.Http.Authentication;
+using Microsoft.AspNet.Mvc;
+
+namespace GraphFilesWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return new ChallengeResult(
+                OpenIdConnectDefaults.AuthenticationScheme, new AuthenticationProperties { RedirectUri = "/" });
+        }
+
+        public async Task<IActionResult> SignOut()
+        {
+            var callbackUrl = Url.Action("SignOutCallback", "Account", values: null, protocol: Request.Scheme);
+            await HttpContext.Authentication.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.Authentication.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme,
+                new AuthenticationProperties { RedirectUri = callbackUrl });
+            return new EmptyResult();
+        }
+
+        public IActionResult SignOutCallback()
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                // Redirect to home page if the user is authenticated.
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ O3653/O3653-3 Deep Dive into Office 365 APIs for OneDrive for Business/Completed Projects/ADALVersion/src/GraphFilesWeb/Controllers/FilesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Authorization;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GraphFilesWeb.Controllers
+{
+    public class FilesController : Controller
+    {
+        [Authorize]
+        public async Task<ActionResult> Index(int? pageSize)
+        {
+            FileRepository repository = new FileRepository(accessToken: null);
+
+            // setup paging defaults if not provided
+            pageSize = pageSize ?? 10;
+
+            // setup paging for the IU
+            ViewBag.PageSize = pageSize.Value;
+
+            var results = await repository.GetMyFilesAsync(pageSize.Value);
+            return View(results);
+        }
+
+        [Authorize]
+        public async Task<ActionResult> Delete(string id, string etag)
+        {
+            FileRepository repository = new FileRepository(accessToken: null);
+            if (id != null)
+            {
+                await repository.DeleteItemAsync(id, etag);
+            }
+
+            return Redirect("/Files");
+        }
+    }
+}
