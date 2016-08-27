f7af128f26eae55103c7a6bd608362ced38144d2
Diane Diaz <ddiaz@microsoft.com>
2016-08-26 19:34:03 -0700

    updated completed lab

--- O3653/O3653-19 WebHooks/Completed Projects/GraphWebhooks/GraphWebhooks/Auth/AuthHelper.cs
+++ O3653/O3653-19 WebHooks/Completed Projects/GraphWebhooks/GraphWebhooks/Auth/AuthHelper.cs
@@ -7,8 +7,10 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Web;
+using System.Security.Claims;
 using GraphWebhooks.TokenStorage;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
 
 namespace GraphWebhooks.Auth
 {
@@ -16,45 +18,115 @@ namespace GraphWebhooks.Auth
     {
         // This is the logon authority
         // i.e. https://login.microsoftonline.com/common
-        public string Authority { get; set; }
+        public string Authority = string.Format(System.Configuration.ConfigurationManager.AppSettings["ida:AADInstance"], "common", "");
         // This is the application ID obtained from registering at
         // https://apps.dev.microsoft.com
-        public string AppId { get; set; }
+        private string AppId = System.Configuration.ConfigurationManager.AppSettings["ida:AppId"];
         // This is the application secret obtained from registering at
         // https://apps.dev.microsoft.com
-        public string AppSecret { get; set; }
+        private string AppSecret = System.Configuration.ConfigurationManager.AppSettings["ida:AppSecret"];
         // This is the token cache
-        public SessionTokenCache TokenCache { get; set; }
+        public RuntimeTokenCache TokenCache { get; set; }
 
-        public AuthHelper(string authority, string appId, string appSecret, SessionTokenCache tokenCache)
+        public AuthHelper(RuntimeTokenCache tokenCache)
         {
-            Authority = authority;
-            AppId = appId;
-            AppSecret = appSecret;
             TokenCache = tokenCache;
+        }
+
+        // Makes a POST request to the token endopoint to get an access token using either
+        // an authorization code or a refresh token. This will also add the tokens
+        // to the local cache.
+        public async Task<TokenRequestSuccessResponse> GetTokensFromAuthority(string grantType, string grantParameter, string redirectUri)
+        {
+            // Build the token request payload
+            FormUrlEncodedContent tokenRequestForm = new FormUrlEncodedContent(
+              new[]
+              {
+          new KeyValuePair<string,string>("grant_type", grantType),
+          new KeyValuePair<string,string>("code", grantParameter),
+          new KeyValuePair<string,string>("client_id", this.AppId),
+          new KeyValuePair<string,string>("client_secret", this.AppSecret),
+          new KeyValuePair<string,string>("redirect_uri", redirectUri)
+              }
+            );
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string requestString = tokenRequestForm.ReadAsStringAsync().Result;
+                StringContent requestContent = new StringContent(requestString);
+                requestContent.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
+                // Set up the HTTP POST request
+                HttpRequestMessage tokenRequest = new HttpRequestMessage(HttpMethod.Post, this.Authority + "/oauth2/v2.0/token");
+                tokenRequest.Content = requestContent;
+                tokenRequest.Headers.UserAgent.Add(new ProductInfoHeaderValue("GraphWebhooks", "1.0"));
+                tokenRequest.Headers.Add("client-request-id", Guid.NewGuid().ToString());
+                tokenRequest.Headers.Add("return-client-request-id", "true");
+
+                // Send the request and read the JSON body of the response
+                HttpResponseMessage response = await httpClient.SendAsync(tokenRequest);
+                JObject jsonResponse = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+                JsonSerializer jsonSerializer = new JsonSerializer();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    // Parse the token response
+                    TokenRequestSuccessResponse s = (TokenRequestSuccessResponse)jsonSerializer.Deserialize(
+                      new JTokenReader(jsonResponse), typeof(TokenRequestSuccessResponse));
+
+                    // Save the tokens
+                    TokenCache.UpdateTokens(s);
+                    return s;
+                }
+                else
+                {
+                    // Parse the error response
+                    TokenRequestErrorResponse e = (TokenRequestErrorResponse)jsonSerializer.Deserialize(
+                      new JTokenReader(jsonResponse), typeof(TokenRequestErrorResponse));
+
+                    // Throw the error description
+                    throw new Exception(e.Description);
+                }
+            }
         }
 
         public async Task<string> GetUserAccessToken(string redirectUri)
         {
-            AuthenticationContext authContext = new AuthenticationContext(Authority, false, TokenCache);
+            if (null == TokenCache || null == TokenCache.Tokens)
+            {
+                HttpContext.Current.Request.GetOwinContext().Authentication.Challenge(
+                  new AuthenticationProperties() { RedirectUri = redirectUri },
+                  OpenIdConnectAuthenticationDefaults.AuthenticationType);
 
-            ClientCredential credential = new ClientCredential(AppId, AppSecret);
+                return null;
+            }
 
-            AuthenticationResult authResult = await authContext.AcquireTokenSilentAsync("https://graph.microsoft.com", credential,
-              new UserIdentifier(TokenCache.UserObjectId, UserIdentifierType.UniqueId));
-            return authResult.AccessToken;
+            // If the token is expired, use refresh token to obtain
+            // a new one before returning
+            if (TokenCache.Tokens.ExpiresOn < DateTime.UtcNow)
+            {
+                var response = await GetTokensFromAuthority("refresh_token", TokenCache.Tokens.RefreshToken, redirectUri);
+                TokenCache.UpdateTokens(response);
+                return response.AccessToken;
+            }
+
+            return TokenCache.Tokens.AccessToken;
         }
 
-        public async Task<AuthenticationResult> GetUserTokens(string redirectUri)
+        public static string GetUserId(ClaimsPrincipal userPrincipal)
         {
-            AuthenticationContext authContext = new AuthenticationContext(Authority, false, TokenCache);
+            Claim objId = userPrincipal
+              .FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier");
 
-            ClientCredential credential = new ClientCredential(AppId, AppSecret);
+            if (null != objId)
+            {
+                return objId.Value;
+            }
 
-            return await authContext.AcquireTokenSilentAsync("https://graph.microsoft.com", credential,
-              new UserIdentifier(TokenCache.UserObjectId, UserIdentifierType.UniqueId));
-           
+            Claim nameId = userPrincipal
+              .FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier");
+
+            return (null != nameId) ? nameId.Value : string.Empty;
         }
-
-     }
+    }
 }
--- O3653/O3653-19 WebHooks/Completed Projects/GraphWebhooks/GraphWebhooks/Controllers/SubscriptionController.cs
+++ O3653/O3653-19 WebHooks/Completed Projects/GraphWebhooks/GraphWebhooks/Controllers/SubscriptionController.cs
@@ -1,19 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using GraphWebhooks.Auth;
 using GraphWebhooks.Models;
-using GraphWebhooks.TokenStorage;
 using Newtonsoft.Json;
 using System.Configuration;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
-using Microsoft.Owin.Security;
-using Microsoft.Owin.Security.OpenIdConnect;
+using GraphWebhooks.Auth;
+using System.Security.Claims;
+using GraphWebhooks.TokenStorage;
 
 namespace GraphWebhooks.Controllers
 {
@@ -30,50 +26,36 @@ namespace GraphWebhooks.Controllers
         public async Task<ActionResult> CreateSubscription()
         {
 
+            // Build the request.
+            HttpClient client = new HttpClient();
+            string subscriptionsEndpoint = "https://graph.microsoft.com/v1.0/subscriptions/";
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, subscriptionsEndpoint);
+
+            var subscription = new Subscription
+            {
+                Resource = "me/mailFolders('Inbox')/messages",
+                ChangeType = "created",
+                NotificationUrl = ConfigurationManager.AppSettings["ida:NotificationUrl"],
+                ClientState = Guid.NewGuid().ToString(),
+                ExpirationDateTime = DateTime.UtcNow + new TimeSpan(0, 0, 4230, 0)
+            };
+            string contentString = JsonConvert.SerializeObject(subscription, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+            request.Content = new StringContent(contentString, System.Text.Encoding.UTF8, "application/json");
+
             // Get an access token and add it to the client.
-            AuthenticationResult authResult = null;
             try
             {
-                string userObjId = System.Security.Claims.ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
-                string tenantId = System.Security.Claims.ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid").Value;
-                string authority = string.Format(ConfigurationManager.AppSettings["ida:AADInstance"], tenantId, "");
-                AuthenticationContext authContext = new AuthenticationContext(authority, false);
-                ClientCredential credential = new ClientCredential(ConfigurationManager.AppSettings["ida:AppId"], ConfigurationManager.AppSettings["ida:AppSecret"]);
-                try
-                {
-                    authResult = await authContext.AcquireTokenSilentAsync("https://graph.microsoft.com", credential,
-                                    new UserIdentifier(userObjId, UserIdentifierType.UniqueId));
-                }
-                catch (AdalSilentTokenAcquisitionException)
-                {
-                    Request.GetOwinContext().Authentication.Challenge(new AuthenticationProperties { RedirectUri = "/Subscription/CreateSubscription" },
-                                               OpenIdConnectAuthenticationDefaults.AuthenticationType);
-                    return new EmptyResult();
-                }
+                string userObjId = AuthHelper.GetUserId(ClaimsPrincipal.Current);
+                AuthHelper authHelper = new AuthHelper(new RuntimeTokenCache(userObjId));
+                string accessToken = await authHelper.GetUserAccessToken("/");
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             }
             catch (Exception ex)
             {
                 return RedirectToAction("Index", "Error", new { message = ex.Message, debug = ex.StackTrace });
             }
-
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authResult.AccessToken);
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            // Build the request.
-            string subscriptionsEndpoint = "https://graph.microsoft.com/beta/subscriptions/";
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, subscriptionsEndpoint);
-            var subscription = new Subscription
-            {
-                Resource = "me/mailFolders('Inbox')/messages",
-                ChangeType = "Created",
-                NotificationUrl = ConfigurationManager.AppSettings["ida:NotificationUrl"],
-                ClientState = Guid.NewGuid().ToString(),
-                ExpirationDateTime = DateTime.UtcNow + new TimeSpan(3, 0, 0, 0)
-            };
-
-            string contentString = JsonConvert.SerializeObject(subscription, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
-            request.Content = new StringContent(contentString, System.Text.Encoding.UTF8, "application/json");
 
             // Send the request and parse the response.
             HttpResponseMessage response = await client.SendAsync(request);
@@ -87,20 +69,21 @@ namespace GraphWebhooks.Controllers
                     Subscription = JsonConvert.DeserializeObject<Subscription>(stringResult)
                 };
 
-                // This app temporarily stores the current subscription ID, refreshToken and client state. 
-                // These are required so the NotificationController, which is not authenticated can retrieve an access token keyed from subscription id
-                // Production apps typically use some method of persistent storage.
-                HttpRuntime.Cache.Insert("subscriptionId_" + viewModel.Subscription.Id, 
-                    Tuple.Create(viewModel.Subscription.ClientState, authResult.RefreshToken), null, DateTime.MaxValue, new TimeSpan(24, 0, 0), System.Web.Caching.CacheItemPriority.NotRemovable, null);
 
-                // Save the latest subscription ID, so we can delete it later and filter teh view on it.
+                // This app temporarily stores the current subscription ID, client state, and user object ID. 
+                // These are required so the NotificationController, which is not authenticated, can retrieve an access token from the cache.
+                // Production apps typically use some method of persistent storage.
+                HttpRuntime.Cache.Insert("subscriptionId_" + viewModel.Subscription.Id,
+                    Tuple.Create(viewModel.Subscription.ClientState, AuthHelper.GetUserId(ClaimsPrincipal.Current)), null, DateTime.MaxValue, new TimeSpan(24, 0, 0), System.Web.Caching.CacheItemPriority.NotRemovable, null);
+
+                // Save the latest subscription ID, so we can delete it later and filter the view on it.
                 Session["SubscriptionId"] = viewModel.Subscription.Id;
                 return View("Subscription", viewModel);
-
             }
             else
             {
-                return RedirectToAction("Index", "Error", new { message = response.StatusCode, debug = await response.Content.ReadAsStringAsync() });
+                string debugString = await response.Content.ReadAsStringAsync();
+                return RedirectToAction("Index", "Error", new { message = response.StatusCode, debug = debugString });
             }
         }
 
@@ -108,44 +91,28 @@ namespace GraphWebhooks.Controllers
         [Authorize]
         public async Task<ActionResult> DeleteSubscription()
         {
-            string subscriptionId = (string) Session["SubscriptionId"];
+            // Build the request.
+            HttpClient client = new HttpClient();
+            string serviceRootUrl = "https://graph.microsoft.com/v1.0/subscriptions/";
 
+            string subscriptionId = (string)Session["SubscriptionId"];
             if (!string.IsNullOrEmpty(subscriptionId))
             {
-                string serviceRootUrl = "https://graph.microsoft.com/beta/subscriptions/";
 
                 // Get an access token and add it to the client.
-                string accessToken;
                 try
                 {
-                    string userObjId = System.Security.Claims.ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
-                    string tenantId = System.Security.Claims.ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid").Value;
-                    string authority = string.Format(ConfigurationManager.AppSettings["ida:AADInstance"], tenantId, "");
-                    AuthenticationContext authContext = new AuthenticationContext(authority, false);
-                    ClientCredential credential = new ClientCredential(ConfigurationManager.AppSettings["ida:AppId"], ConfigurationManager.AppSettings["ida:AppSecret"]);
-                    AuthenticationResult authResult = null;
-                    try
-                    {
-                        authResult = await authContext.AcquireTokenSilentAsync("https://graph.microsoft.com", credential,
-                                        new UserIdentifier(userObjId, UserIdentifierType.UniqueId));
-                    }
-                    catch (AdalSilentTokenAcquisitionException)
-                    {
-                        Request.GetOwinContext().Authentication.Challenge(new AuthenticationProperties { RedirectUri = "/Subscription/DeleteSubscription" },
-                                                   OpenIdConnectAuthenticationDefaults.AuthenticationType);
-                        return new EmptyResult();
-                    }
-                    accessToken = authResult?.AccessToken;
+                    string userObjId = AuthHelper.GetUserId(ClaimsPrincipal.Current);
+                    AuthHelper authHelper = new AuthHelper(new RuntimeTokenCache(userObjId));
+                    string accessToken = await authHelper.GetUserAccessToken("/");
 
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 }
                 catch (Exception ex)
                 {
-                    return RedirectToAction("Index", "Error", new { message = ex.Message, debug = "" });
+                    return RedirectToAction("Index", "Error", new { message = ex.Message, debug = ex.StackTrace });
                 }
-
-                HttpClient client = new HttpClient();
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
                 // Send the 'DELETE /subscriptions/id' request.
                 HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Delete, serviceRootUrl + subscriptionId);
@@ -153,11 +120,11 @@ namespace GraphWebhooks.Controllers
 
                 if (!response.IsSuccessStatusCode)
                 {
-                    return RedirectToAction("Index", "Error", new { message = response.StatusCode, debug = response.Content.ReadAsStringAsync() });
+                    string debugString = await response.Content.ReadAsStringAsync();
+                    return RedirectToAction("Index", "Error", new { message = response.StatusCode, debug = debugString });
                 }
             }
             return RedirectToAction("SignOut", "Account");
         }
-
     }
 }
